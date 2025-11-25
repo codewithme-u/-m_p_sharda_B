0006904551b000933b6d16eb87df53c0cb367d1c
codewithme-u <saksham.pokhrel.9821.myself@gmail.com>
2025-11-26 03:28:15 +0530

    Create PingController.java

---
+++ src/main/java/com/mp/controller/PingController.java
@@ -0,0 +1,14 @@
+// simple PingController
+package com.mp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("OK");
+    }
+}
