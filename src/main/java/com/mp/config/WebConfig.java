8b617356d4821ce3e4269d43641c5f00dc8130c8
codewithme-u <saksham.pokhrel.9821.myself@gmail.com>
2025-11-26 04:18:38 +0530

    Remove CORS mappings from WebConfig
    Removed CORS configuration from WebConfig.

--- src/main/java/com/mp/config/WebConfig.java
+++ src/main/java/com/mp/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.mp.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,16 +8,6 @@ import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")     // allow all origins
-                .allowedMethods("*")            // allow all HTTP methods
-                .allowedHeaders("*")            // allow all headers
-                .allowCredentials(true)         // allow cookies/tokens
-                .maxAge(3600);                  // CORS preflight cache duration
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
